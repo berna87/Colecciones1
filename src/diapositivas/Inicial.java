988b6e462c9fa82a5ba6757d04360acc28873663
Bernabe Fernandez <bernillatorres@gmail.com>
2015-01-15 15:35:23 +0100

    cambios

--- src/diapositivas/Inicial.java
+++ src/diapositivas/Inicial.java
@@ -14,14 +14,29 @@ public class Inicial {
 			numeros [i] = in.nextDouble();	
 		}
 		in.close();
+		//imprimir utilizando un bucle
 		System.out.println("Opcion 1:");
 		for (int i = 0; i < TAMARRAY; i++) {
 			System.out.print(numeros[i]+"\t");
 		}
+		//imprimir utilizando un bucle extendido
 		System.out.println("\nOpcion 2:");
 		for (double d : numeros) {
 			System.out.print(d+"\t");
 		}
+		//calcular valor medio
+		double suma=0;
+		for (int i = 0; i < TAMARRAY; i++) {
+			suma+=numeros[i];
+		}
+		double media = suma/TAMARRAY;
+		System.out.printf("%nEl valor medio es %.2f",media);
+		//calcular numero de elementos por debajo de la media
+		int contador = 0;
+		for (int i = 0; i < TAMARRAY; i++) {
+			if(numeros[i] < media) contador++;
+		}
+		System.out.println("\nNumero de valores por debajo de la media: "+contador);
 	}
 
 }
