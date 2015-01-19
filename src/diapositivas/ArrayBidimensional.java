94c910fbf778247ce929d8cec60ba58f1c96d247
Bernabe Fernandez <bernillatorres@gmail.com>
2015-01-19 11:54:03 +0100

    array invertido

--- src/diapositivas/ArrayBidimensional.java
+++ src/diapositivas/ArrayBidimensional.java
@@ -2,7 +2,7 @@ package diapositivas;
 
 public class ArrayBidimensional {
 	public static void main(String[] args) {
-		double [][] tabla = {{0,1,2},{2,3,5},{2,5,4},{2,5,5}};
+		double [][] tabla = {{7,11,2.6},{4.5,13,1},{20,5.8,14},{2,5.9,15}};
 		double suma = 0;
 		int contador = 0;
 		for (int i = 0; i < tabla.length; i++) {
@@ -15,11 +15,20 @@ public class ArrayBidimensional {
 		}
 		double media = suma /contador;	
 		System.out.printf("\nMedia de los valores = %.3f%n",media);
+		System.out.println("-----------------------------");
+		System.out.println("Imprimo tabla2 con valores invertidos de tabla");
+		double [][]tabla2 = new double [4][3];
+		int fl = 0;
+		int col = 0;
 		for (int i = tabla.length-1; i >= 0; i--) {
+			col = 0;
 			for (int j = tabla[i].length-1; j >= 0; j--) {
-				System.out.print(tabla[i][j]+"\t");
+				tabla2 [fl][col] = tabla [i][j];
+				System.out.print(tabla2[fl][col]+"\t");
+				col++;
 			}
-			System.out.println("");
+			fl++;
+			System.out.println();
 		}
 	}
 }
